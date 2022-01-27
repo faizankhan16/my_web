738f84559a1a95507300243da9ccbad05f07f44e
taimuraamer <taimur.aamer@gmail.com>
2022-01-27 15:13:18 +0500

    API Practice

--- myapi/fetch_data.js
+++ myapi/fetch_data.js
@@ -1,3 +1,8 @@
 const axios = require('axios');
 
+const getData = async () => {
+    const data = await axios.get("https://opentdb.com/api.php?amount=10&category=19&difficulty=medium&type=boolean");
+    console.log(data)
+}
 
+getData();
